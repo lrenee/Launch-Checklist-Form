8bf107fa428f1e6275c72ea48c78916864b19d28
lrenee <lrenee.mo@sbcglobal.net>
2019-10-13 10:14:22 -0500

    faulty items checklist issue

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 // Write your JavaScript code here!
 
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+      let pilotNameInput = document.querySelector("input[name = pilotName]");
+      let copilotNameInput = document.querySelector("input[name=copilotName]");
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+      let cargoMassInput = document.querySelector("input[name=cargoMass]");
+      // let faultytItems = document.getElementById("faultyItems");
+      if (pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
+         alert("All fields are required!");
+         event.preventDefault();
+      } else if (!isNaN(pilotNameInput.value) || !isNaN(copilotNameInput.value) || isNaN(fuelLevelInput.value) || isNaN(cargoMassInput.value)) {
+         alert("Please make sure to enter valid information for each field!");
+         event.preventDefault();
+      } else {
+         let faultyItems = document.getElementById("faultyItems");
+         faultyItems.style.visibility = "visible";
+         console.log(faultyItems.style.visibility);
+         console.log("hello");
+         let pilotStatus = document.getElementById("pilotStatus");
+         pilotStatus.innerHTML = `Pilot ${pilotNameInput.value} is ready for launch`;
+      };
+   });
+});
+
+
+
+
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
